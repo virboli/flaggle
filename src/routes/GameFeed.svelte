26dc0b9580ed79d057b32d6567dd3335f401df64
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-05 22:35:28 -0400

    feat: make sizing responsive

--- src/routes/GameFeed.svelte
+++ src/routes/GameFeed.svelte
@@ -13,7 +13,9 @@
 
 <div class="flex flex-col gap-4 my-4 overflow-auto">
   {#each items as country, i}
-    <div class="relative flex h-48 bg-base-200/50 items-end rounded-box rounded-r-none">
+    <div
+      class="relative flex h-[20vw] min-h-20 max-h-48 bg-base-200/50 items-end rounded-box rounded-r-none"
+    >
       <p class="absolute top-0 left-0 m-4 opacity-25 text-5xl">{items.length - i}</p>
       <p class="m-4">{country.name}</p>
       <img
