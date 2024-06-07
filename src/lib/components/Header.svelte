50b787e2e7b9102c257ea89e8cc09a90bcaf4e36
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-06 23:35:17 -0400

    feat: tweak header layout

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,11 +2,14 @@
   import { base } from "$app/paths";
 </script>
 
-<header class="sticky top-0 z-10 flex flex-col items-center p-2 bg-base-200">
-  <a href="{base}/"
-    ><h1 class="text-xl">Flaggle <i class="text-base-content/50">kennyhui.dev</i></h1></a
-  >
-  <div class="flex gap-4">
-    <a href="{base}/daily" class="link">Daily</a>
+<header class="sticky top-0 z-10 flex flex-col p-2 bg-base-200">
+  <div class="grid grid-cols-[1fr_auto_1fr]">
+    <div></div>
+    <a href="{base}/"
+      ><h1 class="text-xl">Flaggle <i class="text-base-content/50">kennyhui.dev</i></h1></a
+    >
+    <div class="flex gap-4 justify-center items-center">
+      <a href="{base}/daily" class="link">Daily</a>
+    </div>
   </div>
 </header>
