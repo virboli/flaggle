407a364e9f7add2ea23112d3f737869dfcb3ba03
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-09 21:59:38 -0400

    fix: redo delta day function

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,7 @@
 export function getDeltaDay(date: string): number {
   const DAY: number = 86_400_000;
   const TIMEZONE_OFFSET: number = new Date().getTimezoneOffset() * 60_000;
-  const target: number = Math.floor((new Date(date).getTime() + TIMEZONE_OFFSET) / DAY);
-  const now: number = Math.floor(Date.now() / DAY);
-  return target - now;
+  const a = new Date().getTime() - TIMEZONE_OFFSET;
+  const b = new Date(date).getTime();
+  return Math.floor(b / DAY) - Math.floor(a / DAY);
 }
