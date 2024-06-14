2380ecbc48cd5ae41fabfafd7d65dbe494a6f510
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-13 21:51:35 -0400

    fix: replace error icon

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import BiExclamationOctagon from "~icons/bi/exclamation-octagon";
+  import LucideOctagonAlert from "~icons/lucide/octagon-alert";
 </script>
 
 <div class="flex flex-col items-center justify-center gap-2 h-full">
-  <span class="text-5xl mb-2"><BiExclamationOctagon></BiExclamationOctagon></span>
+  <span class="text-5xl mb-2"><LucideOctagonAlert></LucideOctagonAlert></span>
   <h2 class="text-4xl">{$page.status}</h2>
   <p class="text-neutral-content">{$page.error?.message}</p>
 </div>
