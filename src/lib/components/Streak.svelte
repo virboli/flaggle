8b8bf41ec9976ef8f742baaf7062726e04e850cc
Kenny <101839505+khui0@users.noreply.github.com>
2024-06-28 17:42:20 -0400

    feat: color streak when over 10

--- src/lib/components/Streak.svelte
+++ src/lib/components/Streak.svelte
@@ -5,6 +5,16 @@
 
 {#if value > 0}
   <div class="bg-base-300 px-3 self-stretch rounded-btn flex items-center">
-    <p class="font-bold">{value.toLocaleString()} {postfix}</p>
+    <p class="font-bold">
+      {#if value >= 10}
+        <span
+          class="bg-gradient-to-b from-[#df4223] via-[#ed7150] to-[#fae070] text-transparent bg-clip-text"
+          >{value.toLocaleString()}
+        </span>
+      {:else}
+        {value.toLocaleString()}
+      {/if}
+      {postfix}
+    </p>
   </div>
 {/if}
