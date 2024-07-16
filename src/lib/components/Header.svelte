b1a13739664f1e9219d36db5c9ef9d943443f6eb
Kenny <101839505+khui0@users.noreply.github.com>
2024-07-16 11:01:49 -0400

    feat: game mode in title

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,11 +10,27 @@
   import GameModes from "./GameModes.svelte";
 
   import LucideSettings from "~icons/lucide/settings";
+
+  let gameMode: string;
+
+  page.subscribe((page) => {
+    gameMode = (() => {
+      switch (page.url.pathname) {
+        case base + "/classic":
+          return "Classic";
+        case base + "/lightning":
+          return "Lightning";
+        case base + "/daily":
+          return "Daily";
+      }
+      return "";
+    })();
+  });
 </script>
 
 <header class="sticky top-0 z-10 p-2 bg-base-200 grid grid-cols-[1fr_auto_1fr]">
   <div class="flex justify-start items-center h-full">
-    {#if $page.url.pathname === base + "/"}
+    {#if $page.url.pathname === base + "/classic"}
       <Streak value={$streak}></Streak>
     {:else if $page.url.pathname === base + "/lightning"}
       <Streak value={$lightningStreak}></Streak>
@@ -23,7 +39,12 @@
     {/if}
   </div>
   <a href="{base}/">
-    <h1 class="text-xl font-bold active:scale-95 transition-transform">Flaggle</h1>
+    <h1 class="text-xl font-bold active:scale-95 transition-transform">
+      Flaggle
+      {#if gameMode}
+        <span class="font-normal"> {gameMode}</span>
+      {/if}
+    </h1>
   </a>
   <div class="flex justify-end items-center h-full">
     <GameModes></GameModes>
