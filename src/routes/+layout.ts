267cb5612842e3dadc589dcc3a6e7c1984320711
Kenny <101839505+khui0@users.noreply.github.com>
2024-08-22 17:35:10 -0400

    feat: analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,3 +2,8 @@ export const prerender = true;
 import "../app.css";
 import "@fontsource-variable/figtree";
 import "$lib/theme";
+
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+
+inject({ mode: dev ? "development" : "production" });
