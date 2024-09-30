8b4ccd0bfdbd532f491c27f0ea6f6db7e524f82c
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-30 16:22:25 -0400

    feat: purple streak

--- src/lib/components/Streak.svelte
+++ src/lib/components/Streak.svelte
@@ -4,14 +4,19 @@
 </script>
 
 <p class="font-bold">
-  <span class:fire={value >= 10}>{value.toLocaleString()} </span>
+  <span class:fire={value >= 10} class:purple={value >= 50}>{value.toLocaleString()} </span>
   <span class="glow">{postfix}</span>
 </p>
 
 <style>
-  .fire {
+  .red {
     @apply bg-gradient-to-b from-[#df4223] via-[#ed7150] to-[#fae070] text-transparent bg-clip-text;
     text-shadow: 0 0 10px red;
+  }
+
+  .purple {
+    @apply bg-gradient-to-b from-[#7b23df] via-[#c850ed] to-[#fa70b7] text-transparent bg-clip-text;
+    text-shadow: 0 0 10px #c850ed;
   }
 
   .glow {
