a5d1c9bd510bf2290e683ef56a3e07e540a9d93f
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-30 15:52:35 -0400

    refactor: name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  import MainGameMode from "$lib/components/MainGameMode.svelte";
+  import MainMenuItem from "$lib/components/MainMenuItem.svelte";
 
   import LucideCalendarDays from "~icons/lucide/calendar-days";
   import LucideFlag from "~icons/lucide/flag";
   import LucideZap from "~icons/lucide/zap";
 </script>
 
-<MainGameMode path="daily" title="Daily">
+<MainMenuItem path="daily" title="Daily">
   A new flag every day
   <span slot="icon"><LucideCalendarDays></LucideCalendarDays></span>
-</MainGameMode>
-<MainGameMode path="classic" title="Classic">
+</MainMenuItem>
+<MainMenuItem path="classic" title="Classic">
   Match colors and shapes
   <span slot="icon"><LucideFlag></LucideFlag></span>
-</MainGameMode>
-<MainGameMode path="lightning" title="Lightning">
+</MainMenuItem>
+<MainMenuItem path="lightning" title="Lightning">
   Fast-paced quick identification
   <span slot="icon"><LucideZap></LucideZap></span>
-</MainGameMode>
+</MainMenuItem>
