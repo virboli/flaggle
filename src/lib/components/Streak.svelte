e30fb5e7d1aa1b2aa23bffb3123cd46c95ec14ae
Kenny <101839505+khui0@users.noreply.github.com>
2024-09-30 20:49:40 -0400

    feat: more streak colors

--- src/lib/components/Streak.svelte
+++ src/lib/components/Streak.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
   export let value: number;
   export let postfix: string = "🔥";
+
+  $: effect = (() => {
+    if (value >= 1000) {
+      return "rainbow";
+    } else if (value >= 500) {
+      return "yellow";
+    } else if (value >= 250) {
+      return "green";
+    } else if (value >= 100) {
+      return "blue";
+    } else if (value >= 50) {
+      return "purple";
+    } else if (value >= 10) {
+      return "red";
+    }
+  })();
 </script>
 
 <p class="font-bold whitespace-nowrap">
-  <span class:fire={value >= 10} class:purple={value >= 50}>{value.toLocaleString()} </span>
+  <span class={effect}>{value.toLocaleString()} </span>
   <span class="glow">{postfix}</span>
 </p>
 
@@ -17,6 +33,46 @@
   .purple {
     @apply bg-gradient-to-b from-[#7b23df] via-[#c850ed] to-[#fa70b7] text-transparent bg-clip-text;
     text-shadow: 0 0 10px #c850ed;
+  }
+
+  .blue {
+    @apply bg-gradient-to-b from-[#4923df] via-[#50c6ed] to-[#70face] text-transparent bg-clip-text;
+    text-shadow: 0 0 10px #50c6ed;
+  }
+
+  .green {
+    @apply bg-gradient-to-b from-[#23dfa7] via-[#50ed68] to-[#d3fa70] text-transparent bg-clip-text;
+    text-shadow: 0 0 10px #50ed68;
+  }
+
+  .yellow {
+    @apply bg-gradient-to-b from-[#df8123] via-[#e8ed50] to-[#a7fa70] text-transparent bg-clip-text;
+    text-shadow: 0 0 10px #e8ed50;
+  }
+
+  /* Adapted from https://codepen.io/shironitus/pen/QWyNBqx */
+  .rainbow {
+    @apply relative text-transparent bg-clip-text;
+    background-image: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+    background-size: 400%;
+    animation: animate 5s linear infinite;
+  }
+
+  .rainbow:before {
+    content: "";
+    @apply absolute inset-x-0 inset-y-2 -z-[1] rounded-full blur-[10px];
+    background: linear-gradient(90deg, #03a9f4, #f441a5, #ffeb3b, #03a9f4);
+    background-size: 400%;
+    animation: animate 5s linear infinite;
+  }
+
+  @keyframes animate {
+    0% {
+      background-position: 0%;
+    }
+    100% {
+      background-position: 400%;
+    }
   }
 
   .glow {
